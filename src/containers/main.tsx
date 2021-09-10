8a34384b335ce8c54bbff8578bddf65e32311fbb
Your Name <hr5359@srmist.edu.in>
2021-09-10 19:22:49 +0530

    ✨ Add 3D models

--- src/containers/main.tsx
+++ src/containers/main.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion"
 import Navbar from '../components/Navbar/Navbar'
 import Home from "../pages/Home/Home"
 import Learn from "../pages/Learn/Learn"
+import ClassRoom1 from "../pages/Classroom/Classroom1"
 import FunZone from "../pages/FunZone/FunZone"
 import TicTacToe from "../pages/FunZone/Games/TicTacToe"
 import Contact from "../pages/Contact/Contact"
@@ -63,8 +64,8 @@ function Main() {
             <Switch>
                 <Route path='/' exact component={Home} />
                 <Route path='/learn' exact component={Learn} />
-                {/* <Route path='/class1' exact component={ClassRoom1} />
-            <Route path='/solarsystemexplore' exact component={SloarSystemExplore} />
+                <Route path='/class1' exact component={ClassRoom1} />
+                {/* <Route path='/solarsystemexplore' exact component={SloarSystemExplore} />
             <Route path='/perseveranceexplore' exact component={PerseveranceExplore} /> */}
                 <Route path='/funzone' exact component={FunZone} />
                 <Route path='/tictactoe' exact component={TicTacToe} />
