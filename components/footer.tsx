b1af9aa577264c025eeca165af4dd22581eac35d
Kshitij Soni <60361593+kshitijsoni@users.noreply.github.com>
2022-09-01 14:58:30 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Footer() {
     return (
         <div className="flex text-white justify-center text-xl py-3 items-center w-full mx-auto z-50">
-            &lt;/&gt; with <Image className="w-8 mb-1" height="25" width="25" src="/love.png" alt="Love" />,<Image src="/next-js.svg" height="20" width="20" alt="Next" /> &nbsp;and &nbsp;<Image src="/typescript.svg" height="20" width="20" alt="TS" /> &nbsp; by &nbsp;<a className="underline" href="https://harshal09.vercel.app/" target="_blank" rel="noreferrer">Harshal Raikwar</a>
+            &lt;/&gt; with <Image className="w-8 mb-1" height="25" width="25" src="/love.png" alt="Love" />,<Image src="/next-js.svg" height="20" width="20" alt="Next" /> &nbsp;and &nbsp;<Image src="/typescript.svg" height="20" width="20" alt="TS" /> &nbsp; by &nbsp;<a className="underline" href="https://harshal09.vercel.app/" target="_blank" rel="noreferrer">Innovation Lab</a>
         </div>
     )
 }
